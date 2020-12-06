c79a9ef61eb6e57aed209093d2d404806a32a58c
Alvarohf <neoncommander@gmail.com>
2020-12-06 01:30:22 +0100

    Updated about section

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -111,6 +111,24 @@ jQuery(document).ready(function($){
         $(this).removeClass('spoiler');
      });
     
+     // gr<ph circles
+     var myCircle = Circles.create({
+      id:                  'circles-1',
+      radius:              60,
+      value:               43,
+      maxValue:            100,
+      width:               10,
+      text:                function(value){return value + '%';},
+      colors:              ['#D3B6C6', '#4B253A'],
+      duration:            400,
+      wrpClass:            'circles-wrp',
+      textClass:           'circles-text',
+      valueStrokeClass:    'circles-valueStroke',
+      maxValueStrokeClass: 'circles-maxValueStroke',
+      styleWrapper:        true,
+      styleText:           true
+    });
+
  });   
 
 // deferred style loading
