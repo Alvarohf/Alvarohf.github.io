6cce4fd8e7fdc25d19291acb9b667c0184601ba3
Alvarohf <neoncommander@gmail.com>
2020-12-08 12:15:28 +0100

    Fixed errors

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -134,7 +134,7 @@ jQuery(document).ready(function($){
       value:               85,
       maxValue:            100,
       width:               10,
-      text:                function(value){High},
+      text:                function(value){"High"},
       colors:              ['#D3B6C6', '#4B253A'],
       duration:            400,
       wrpClass:            'circles-wrp',
